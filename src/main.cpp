cedd1f0d8011ed133ee4bcc7fabdd3f96023c624
consefact <1184503757@qq.com>
2026-02-08 15:13:18 +0800

    learning

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,27 @@
 int main(int argc, char* argv[]) {
     std::string path = argv[1];
     cv::Mat img = cv::imread(path);
-    cv::Mat img_resize, img_crop;
+    cv::Mat img_hsv, mask;
 
-    cv::resize(img, img_resize, cv::Size(), 0.5, 2);
+    cv::cvtColor(img, img_hsv, cv::COLOR_BGR2HSV);
 
-    cv::Rect roi(100, 100, 300, 200);
-    img_crop = img(roi);
-    std::cout << img.size() << std::endl;
+    int hmin = 156, hmax = 180, smin = 43, smax = 255, vmin = 46, vmax = 255;
+    cv::namedWindow("track bar", (400,400));
+    cv::createTrackbar("hmin", "track bar", &hmin, 179);
+    cv::createTrackbar("hmax", "track bar", &hmax, 179);
+    cv::createTrackbar("smin", "track bar", &smin, 255);
+    cv::createTrackbar("smax", "track bar", &smax, 255);
+    cv::createTrackbar("vmin", "track bar", &vmin, 255);
+    cv::createTrackbar("vmax", "track bar", &vmax, 255);
+
+    while(1){
+    cv::Scalar lower(hmin, smin, vmin), upper(hmax, smax, vmax);
+    cv::inRange(img_hsv, lower, upper, mask);
+
     cv::imshow("img", img);
-    cv::imshow("resize", img_resize);
-    cv::imshow("crop", img_crop);
-    cv::waitKey(0);
+    cv::imshow("img_hsv", img_hsv);
+    cv::imshow("mask", mask);
+    cv::waitKey(1);
+    }
+    
 }
