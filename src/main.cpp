6a900efd9265c08cd66012995699dc588a939034
consefact <1184503757@qq.com>
2026-02-08 09:12:14 +0800

    learning

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,16 @@
 
 int main(int argc, char* argv[]) {
     std::string path = argv[1];
-    cv::Mat img_ori = cv::imread(path), img_gray, img_blur, img_cny, img_dil, img_ero;
+    cv::Mat img = cv::imread(path);
+    cv::Mat img_resize, img_crop;
 
-    cv::cvtColor(img_ori, img_gray, cv::COLOR_BGR2GRAY);
-    cv::GaussianBlur(img_ori, img_blur, cv::Size(3, 3), 3, 0);
-    cv::Canny(img_blur, img_cny, 25, 75);
+    cv::resize(img, img_resize, cv::Size(), 0.5, 2);
 
-    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
-    cv::dilate(img_cny, img_dil, kernel);
-    cv::erode(img_dil, img_ero, kernel);
-
-    cv::imshow("blur",img_blur);
-    cv::imshow("gray", img_gray);
-    cv::imshow("canny", img_cny);
-    cv::imshow("dilate", img_dil);
-    cv::imshow("erode", img_ero);
-
+    cv::Rect roi(100, 100, 300, 200);
+    img_crop = img(roi);
+    std::cout << img.size() << std::endl;
+    cv::imshow("img", img);
+    cv::imshow("resize", img_resize);
+    cv::imshow("crop", img_crop);
     cv::waitKey(0);
 }
