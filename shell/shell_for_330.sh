cbb04d999fa7a7fb0745c3a3a4dfe95f06938d5e
consefact <consefact@foxmail.com>
2026-03-03 20:00:24 +0800

    shell_complete

--- shell/shell_for_330.sh
+++ shell/shell_for_330.sh
@@ -1,20 +1,3 @@
 #!/bin/zsh
-
-# 创建会话和第一个窗口
-tmux new-session -d -s ros_session -n color_detect_nodes
-
-# Pane 0: roscore
-tmux send-keys -t ros_session:0 'roscore' C-m
-
-# Pane 1: location.launch
-tmux split-window -h -t ros_session:0
-tmux split-window -v -t ros_session:0.1
-
-tmux send-keys -t ros_session:0.1 'sleep 7; source ~/group2_ws/devel/setup.bash; roslaunch scan_land scan_land.launch' C-m
-tmux send-keys -t ros_session:0.2 'sleep 10; roslaunch scan_land scan_land_py.launch' C-m
-# 整理第一个窗口布局
-tmux select-layout -t ros_session:0 tiled
-
-# 附加到会话并显示第一个窗口
-tmux select-window -t ros_session:0
-tmux attach-session -t ros_session:0
+tmux split-window -h "sleep 10; roslaunch scan_land scan_land_py.launch"
+sleep 7; source ~/group2_ws/devel/setup.bash; roslaunch scan_land scan_land.launch
